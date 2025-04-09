d3a03b5f11a9cd308fdedc9511bb73b1dd8dd2c6
LavanyaM <mantri.lavanya1220@gmail.com>
2025-04-09 02:18:31 -0500

    completed

--- project-task.js
+++ project-task.js
@@ -29,30 +29,32 @@ This activity reinforces:
 */
 
 // ============================================
-// ❌ Original Code (Before Refactoring)
+// nowrefactored
 // ============================================
 
-// Script 1 - Greeting multiple users
+// S - 1
 console.log("Welcome, Alice!");
 console.log("Welcome, Bob!");
 console.log("Welcome, Charlie!");
 
-// Script 2 - Sum calculation
-let num1 = 5, num2 = 10;
-let sum = num1 + num2;
+// S - 2
+
+let numA = 5, numB = 10;
+let sum = numA + numB;
 console.log("The sum of 5 and 10 is " + sum);
 
-// Script 3 - Product calculation
-let product = num1 * num2;
+// S - 3 
+
+let product = numA * numB;
 console.log("The product of 5 and 10 is " + product);
 
-// Script 4 - Print names from a list
+// S - 4
+
 let names = ["Alice", "Bob", "Charlie"];
 console.log("Names in the list:");
 for (let i = 0; i < names.length; i++) {
     console.log(names[i]);
 }
-
 /*
 ===========================================
 🛠️ Steps for Refactoring
@@ -72,3 +74,50 @@ for (let i = 0; i < names.length; i++) {
 */
 
 // ✅ Your refactored code goes below this line!
+
+// Script 1
+
+function greet(name) {
+   console.log(`Welcome, ${name}!`);
+}
+greet("Alice");
+greet("Bob");
+greet("Charlie");
+
+// Script 2
+
+function add(a, b) {
+   return a + b ;
+}
+
+function printSum(num1, num2) {
+   console.log(`The sum of ${num1} and ${num2} is ${add(num1, num2)}.`);
+}
+let num1 = 5, num2 = 10;
+printSum(num1, num2);
+printSum(6,10);
+printSum(3,7);
+
+// Script 3
+
+function multiply(a, b) {
+   return a * b;
+}
+
+function printProduct(num1, num2) {
+   console.log(`The product of ${num1} and ${num2} is ${multiply(num1, num2)}.`);
+}
+printProduct(num1, num2);
+
+// Script 4
+
+function printNames(listnames) {
+   console.log("Names in the list: ");
+   for(let i = 0; i < listnames.length; i++) {
+      console.log(listnames[i]);
+   }
+}
+let listnames = ["Alice", "Bob", "Charlie"];
+printNames(listnames);
+
+
